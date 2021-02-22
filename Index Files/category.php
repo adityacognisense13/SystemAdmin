112ebb4aaf58657dcb3ba6eda2844e91ee70667f
Aditya <aditya.cognisense13@gmail.com>
2021-02-22 18:04:50 +0530

    renamed index into category

--- Index Files/category.php
+++ Index Files/category.php
@@ -75,7 +75,6 @@ include "../includes/db.php";
 
 
 
-
         <script src="" async defer></script>
         <!-- Boot Strap Code  -->
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
